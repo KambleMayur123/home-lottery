57437501323ab55593f6f2ac0b48b1af3e6197ac
KambleMayur123 <kamblemayur843@gmail.com>
2024-11-29 23:29:24 +0530

    build errors fixed

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
 module.exports = {
   reactStrictMode: true,
-  output: 'export', // Enables static export
-  images: {
-    unoptimized: true // Disable image optimization for static export
-  },
+
 };
